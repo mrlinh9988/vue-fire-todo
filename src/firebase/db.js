a6db29fe489386fe6165f1782ee53e2e4449b701
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2021-05-08 17:14:13 +0700

    update

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -2,17 +2,15 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 // Get a Firestore instance
-
 var firebaseConfig = {
-  apiKey: '<your-api-key>',
-  authDomain: '<your-auth-domain>',
-  databaseURL: '<your-database-url>',
-  projectId: '<your-cloud-firestore-project>',
-  storageBucket: '<your-storage-bucket>',
-  messagingSenderId: '<your-sender-id>',
-  appId: '<your-app-id>',
-  measurementId: '<your-measurement-id>'
-};
+  apiKey: 'AIzaSyAmAovN9MnMtsBpXNqcmc_VlW9IHdAUp6E',
+  authDomain: 'covid-ht.firebaseapp.com',
+  databaseURL: 'https://covid-ht-default-rtdb.firebaseio.com',
+  projectId: 'covid-ht',
+  storageBucket: 'covid-ht.appspot.com',
+  messagingSenderId: '504282301512',
+  appId: '1:504282301512:web:697f9bdccabf13cbe0847d'
+}
 
 export const db = firebase.initializeApp(firebaseConfig).firestore();
 
